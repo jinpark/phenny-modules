2606cefd11e8b3e918237b809bf68d1bd1a36729
Jin Park <jin+github@jinpark.net>
2015-12-30 20:33:06 +0900

    try relative import

--- bobkov.py
+++ bobkov.py
@@ -4,12 +4,21 @@ markov.py - bob markov Module
 """
 
 from sopel.module import commands, example
-from markov_text.db import Db
-from markov_text.gen import Generator
-from markov_text.parse import Parser
-from markov_text.sql import Sql
-from markov_text.rnd import Rnd
+import os
 import sys
+try:
+    from markov_text.db import Db
+    from markov_text.gen import Generator
+    from markov_text.parse import Parser
+    from markov_text.sql import Sql
+    from markov_text.rnd import Rnd
+except:
+    sys.path.append(os.path.join(os.path.dirname(__file__), "markov_text"))
+    from markov_text.db import Db
+    from markov_text.gen import Generator
+    from markov_text.parse import Parser
+    from markov_text.sql import Sql
+    from markov_text.rnd import Rnd
 import sqlite3
 import codecs
 
