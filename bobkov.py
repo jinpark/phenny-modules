55b3fe4f63f3000dac0bcaef6951092430184a20
Jin Park <jin+github@jinpark.net>
2015-12-31 17:55:51 +0900

    whoops, left self in function

--- bobkov.py
+++ bobkov.py
@@ -139,7 +139,7 @@ class Generator:
             sentence.pop(0)
 
         if len(sentence) < 3 and retry > 0:
-            self.generate(self, word_separator, starting_word, retry - 1)
+            self.generate(word_separator, starting_word, retry - 1)
 
         return word_separator.join(sentence[:1-depth])
 
