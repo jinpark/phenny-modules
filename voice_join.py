3fdc72a62cf192843557e50b9c3570e171e455e5
Jin Park <jin@jinpark.net>
2019-01-07 12:36:15 +0900

    add voice_join

---
+++ voice_join.py
@@ -0,0 +1,22 @@
+
+# -*- coding: utf8 -*-
+"""
+voide_join.py - Willie giphy Module
+"""
+
+from sopel.module import commands, rule, event
+import threading
+
+def give_voice(bot, trigger, username);
+    channel = trigger.sender
+    print(['give voice ran with', channel, username])
+    return bot.write(['MODE', channel, '+v',username])
+
+@event('JOIN')
+@rule(r'.*')
+def voice_join(bot, trigger):
+    if  trigger.nick == bot.nick or trigger.isop or trigger.isvoice:
+        return
+    timer = Threading.timer(30.0, give_voice, args=[bot, trigger, trigger.nick])
+    
+    
