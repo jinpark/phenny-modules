3ea795f05f09b9b23d0ea0371d22638c5d6ba5d9
Jin Park <jin@jinpark.net>
2020-05-04 10:37:44 -0400

    fix giphy when it fails

--- giphy.py
+++ giphy.py
@@ -18,9 +18,13 @@ def giphy(bot, trigger):
     payload = {'api_key': API_KEY, 'q': user_input, 'offset': 0, 'limit': 1, 'rating': 'PG-13'}
     r = requests.get("http://api.giphy.com/v1/gifs/search", params=payload)
     giphy_json = r.json()
-    if giphy_json['pagination']['count'] > 0:
-        rand_image = random.randint(0, giphy_json['pagination']['count'] - 1 )
-        image_url = giphy_json['data'][rand_image]['images']['original']['url']
-        bot.say(image_url)
-    else:
+    try:
+        if giphy_json['pagination']['count'] > 0:
+            rand_image = random.randint(0, giphy_json['pagination']['count'] - 1 )
+            image_url = giphy_json['data'][rand_image]['images']['original']['url']
+            bot.say(image_url)
+        else:
+            bot.say('No gif found. Blame bob')
+    except:
         bot.say('No gif found. Blame bob')
+
